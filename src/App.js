7d058fd47f1778e80f770016b87604bc4db5a136
alexsaltstein <asaltste@stevens.edu>
2019-11-13 16:02:21 -0500

    removed unneccesary code

--- src/App.js
+++ src/App.js
@@ -6,22 +6,6 @@ import { Carousel } from 'react-responsive-carousel';
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
     <div className="main">
       <div className="row movie flex-row-reverse" id="monsoon">
         <div className="sidetext col-lg">
